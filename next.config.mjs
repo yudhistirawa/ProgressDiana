8603a74ac404c10bce82e29ef292751ed9b6fd9e
unknown <yudhiswa97@gmail.com>
2025-10-09 17:07:21 +0800

    test4

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Allow production builds to succeed even if there are ESLint errors
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
